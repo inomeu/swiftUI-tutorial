560c3d658f1f1b922199e606e87fee0cb1746705
Mei Inoue <inomei.0314@gmail.com>
2022-12-07 16:54:31 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by inoue mei on 2022/12/07.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
